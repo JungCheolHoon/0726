7f20973106d03fc555fdbc29592d8a421c054dc0
JungCheolHoon <cjswovkdnj12@naver.com>
2022-07-26 10:42:46 +0900

    Modify StudentInfo.java

--- src/StudentInfo.java
+++ src/StudentInfo.java
@@ -12,9 +12,13 @@ public class StudentInfo {
 		System.out.print("학번 : ");
 		String hakbun = sc.nextLine();
 		
+		System.out.println("주소 : ");
+		String address = sc.nextLine();
+		
 		System.out.println("날짜 : " + today );
 		System.out.println("이름 : " + name);
 		System.out.println("학번 : " + hakbun);
+		System.out.println("주소 : " + address);
 		
 	}
 }
